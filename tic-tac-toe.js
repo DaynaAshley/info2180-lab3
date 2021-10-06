eb0a8e62da4f8fdca77f0c3f46b92f2bef9c6de4
Dayna-Ashley Roberts <daynaashley.roberts@mymona.uwi.edu>
2021-10-05 19:47:30 -0500

    Added X or O to square to Javascript

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,5 +4,33 @@ document.addEventListener('DOMContentLoaded', () => {
     let boxes= document.querySelectorAll('#board > div');
     for (let i = 0; i < boxes.length; i++) {
         boxes[i].className = "square";
+        boxes[i].addEventListener("click",clickedbox);
     } 
 });
+
+let game_arrayx=[];
+let game_arrayo=[];
+
+let current_play="1";
+
+
+function clickedbox(box){
+    if (box.target.innerHTML.length==0){
+        if (current_play %2 ==0){
+            game_arrayx.push(box.target.id);
+            console.log(game_arrayx);
+            box.target.innerHTML="X";
+            box.target.className="square X"
+        }
+        else{
+            game_arrayo.push(box.target.id);
+            console.log(game_arrayo);
+            box.target.innerHTML="O";
+            box.target.className="square O"
+        }
+    }
+    else{
+        return;
+    }
+    current_play++;
+}
